ced0306f580c1c272b6a1b92c9d4ef1e8ec45227
saulo-data <saulo82faria@gmail.com>
2024-11-01 16:00:14 -0300

    layout-1

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,8 @@
-#from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Home')
-
-def contato(request): 
-    return HttpResponse('contato')
-
-def sobre(request):
-    return HttpResponse('sobre')
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Saulo Faria',
+    })
